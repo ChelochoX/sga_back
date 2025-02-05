2420c5ee35a7cdca2b22f996f067212efde09667
Cesar Lezcano <cesarlezca@gmail.com>
2025-02-05 17:19:26 -0300

    Create CursosAutomapping.cs

---
+++ sga_back/Mappings/CursosAutomapping.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using sga_back.Models;
+using sga_back.Request;
+
+namespace sga_back.Mappings;
+
+public class CursosAutomapping : Profile
+{
+    public CursosAutomapping()
+    {
+        _ = CreateMap<CursoRequest, Curso>();
+    }
+}
